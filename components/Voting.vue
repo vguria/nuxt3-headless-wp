40efc45cb97e6874bd28848f0c62c0f650069956
Victor Gil <victor@vguria.com>
2024-10-09 03:56:57 +0200

    Added shared state to Voting widget

--- components/Voting.vue
+++ components/Voting.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="voting" :class="`absolute top-0 right-0 z-3 m-4 bg-gray-200 hover:bg-red-200 
+  <div :class="`absolute top-0 right-0 z-3 m-4 bg-gray-200 hover:bg-red-200 
   py-1 px-2 rounded-full ${upVoted ? 'bg-red-100' : ''}`"
   @click="toggleUpVoted"
   >
@@ -18,23 +18,33 @@
 </template>
 
 <script setup>
-import { ref } from 'vue';
+import { ref, watch } from 'vue';
+import { useVotedPosts } from '~/composables/votedPosts';
 
 const props = defineProps({
   post: Object
 })
 
-const upVoted = ref(false)
+const { votedPosts, addVotedPost, removeVotedPost, isVotedPost } = useVotedPosts()
+
+const upVoted = ref(isVotedPost(props.post.id))
 const votes = ref(parseInt(props.post.snaxFakeVoteCount))
 
 const toggleUpVoted = () => {
   upVoted.value = !upVoted.value
   if(upVoted.value){
     votes.value += 1
+    addVotedPost(props.post.id)
   } else {
     votes.value -= 1
+    removeVotedPost(props.post.id)
   }
 }
+watch(votedPosts, (newVotedPosts) => {
+  console.log(`${props.post.title} ${isVotedPost(props.post.id)}`)
+  upVoted.value = isVotedPost(props.post.id)
+  votes.value = parseInt(props.post.snaxFakeVoteCount) + (upVoted.value ? 1 : 0)
+}, { deep: true })
 </script>
 
 <style scoped>
--- composables/votedPosts.ts
+++ composables/votedPosts.ts
@@ -1,21 +1,20 @@
 import { ref } from 'vue';
 
-export function useVotedPosts() {
-  const votedPosts = ref<string[]>([]);
-
-  function addVotedPost(postId: string) {
-    if (!votedPosts.value.includes(postId)) {
-      votedPosts.value.push(postId);
-    }
-  }
-
-  function removeVotedPost(postId: string) {
-    votedPosts.value = votedPosts.value.filter(id => id !== postId);
-  }
-
+const votedPosts = ref<string[]>([])
+const addVotedPost = (postId: string) => {
+  votedPosts.value.push(postId)
+}
+const removeVotedPost = (postId: string) => {
+  votedPosts.value = votedPosts.value.filter(id => id !== postId)
+}
+const isVotedPost = (postId: string) => {
+  return votedPosts.value.includes(postId)
+}
+export const useVotedPosts = () => {
   return {
     votedPosts,
     addVotedPost,
-    removeVotedPost
-  };
-}
+    removeVotedPost,
+    isVotedPost,
+  }
+};
